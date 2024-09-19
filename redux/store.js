9fe155bf807697768cc784de522510d60e6c6be4
sandjonyves <sandjonyves@gmail.com>
2024-09-19 01:29:14 +0000

    redux

---
+++ redux/store.js
@@ -0,0 +1,20 @@
+
+import { configureStore } from "@reduxjs/toolkit"
+import { combineReducers } from "@reduxjs/toolkit"
+import { persistStore, persistReducer } from 'redux-persist'
+import {persist}
+
+// import authReducer from "@/redux/slices/authSlice"
+
+
+// const persistConfig = {
+//     key :"root"
+//     storage:st
+// }
+
+
+// const rootReducer = combineReducers({
+//     auth : authReducer
+// })
+
+
